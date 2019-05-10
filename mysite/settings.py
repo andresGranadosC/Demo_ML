2b8f06c8dd8b5b1738f4a5e4371e1344ed3d7439
Andrés Granados <afgranadosc@unal.edu.co>
2019-05-10 15:43:35 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_k*v!kr(96=8q$#01ise2alrbyv9()v_c%m+5&lo+-5(^7w#zn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'demomlservice.herokuapp.com']
 
 
 # Application definition
